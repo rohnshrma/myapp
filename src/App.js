6892868874221d07496b6fec30984d0ee377e31a
rohnshrma <rohn11shrma@gmail.com>
2022-08-01 22:27:41 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-
-import './App.css';
-import Head from "./Heading"
+import "./App.css";
+import Head from "./Heading";
 
 function App() {
   return (
     <div className="App">
-     <Head/>
-     <Head/>
-     <Head/>
-     <Head/>
-     <Head/>
+      <Head
+        title="this is my first post"
+        content="this is some content for box"
+        imgSrc="http://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png"
+      />
+      <Head
+        title="title 2"
+        content="content 2"
+        imgSrc="https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg"
+      />
     </div>
   );
 }
--- src/Heading.js
+++ src/Heading.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./Heading.css"
 
-const Head = () => {
-//   var headStyle = { color: "red" };
-//   var today = new Date().getHours();
-//   if (today > 0 && today <= 12) {
-//     headStyle.color = "orange";
-//   } else if (today > 12 && today <= 6) {
-//     headStyle.color = "yellow";
-//   } else {
-//     headStyle.color = "blue";
-//   }
+const Head = (props) => {
+
+
+    console.log(props)
 
   return (
     <div className="headingBox">
-      {/* <h1 style={headStyle}>Hello im the heading 1</h1> */}
-      <p className="title">Lorem nhelloworld hosihbgbhfgsdb </p>
+    <img src={props.imgSrc} alt="" />
+      <h1 className="title">{props.title}</h1>
+      <p >{props.content} </p>
     </div>
   );
 };
