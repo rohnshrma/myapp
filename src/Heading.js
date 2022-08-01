59f5910c6c808a5bf358e62d57d758cfa9f21464
rohnshrma <rohn11shrma@gmail.com>
2022-08-01 22:06:40 +0530

    jsx and inline styling

---
+++ src/Heading.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Heading.css"
+
+const Head = () => {
+//   var headStyle = { color: "red" };
+//   var today = new Date().getHours();
+//   if (today > 0 && today <= 12) {
+//     headStyle.color = "orange";
+//   } else if (today > 12 && today <= 6) {
+//     headStyle.color = "yellow";
+//   } else {
+//     headStyle.color = "blue";
+//   }
+
+  return (
+    <div className="headingBox">
+      {/* <h1 style={headStyle}>Hello im the heading 1</h1> */}
+      <p className="title">Lorem nhelloworld hosihbgbhfgsdb </p>
+    </div>
+  );
+};
+
+export default Head;
