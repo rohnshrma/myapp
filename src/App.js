1479345bc0d2da768ef58997ee0d6d0fb123f227
rohnshrma <rohn11shrma@gmail.com>
2022-08-01 22:31:37 +0530

    using dummy file for data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import "./App.css";
 import Head from "./Heading";
+import data from "./data.js";
 
 function App() {
   return (
     <div className="App">
-      <Head
-        title="this is my first post"
-        content="this is some content for box"
-        imgSrc="http://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png"
-      />
-      <Head
-        title="title 2"
-        content="content 2"
-        imgSrc="https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg"
-      />
+      <Head title={data[0].title} content={data[0].content} imgSrc ={data[0].imgSrc} />
+      <Head title={data[1].title} content={data[1].content} imgSrc ={data[1].imgSrc} />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,14 @@
+var data = [
+    {
+        title: "title 1",
+        content : "content for title 1",
+        imgSrc : "http://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png"
+    },
+    {
+        title: "title 2",
+        content : "content for title 2",
+        imgSrc : "https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg"
+    }
+]
+
+export default data;
